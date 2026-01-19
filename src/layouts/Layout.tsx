5e70449e2a3ad8d06e4db2be808d3b81a4a47513
Josue herazo <josuedh18@hotmail.es>
2026-01-19 23:13:54 +0100

    arreglando el header con el ig

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -11,16 +11,30 @@ export default function Layout() {
         <header className=" Header flex  justify-between flex-col content-center items-center h-50
          sm:flex-row  mb-5 mx-1  hover:text-yellow-200 shadow-xl bg-black shadow-amber-600
          ">
-            <Link to="/">
-                <img src={latinosvip} alt="Logo" className='text-3xl size-60 w-30 ml-2 h-30 rounded-2xl' />
-            </Link>
-            <h1 className="text-uppercase font-extrabold text-white animate-pulse">Bienvenidos a LatinosVip</h1>
+            <div>
 
-            <div className=' justify-end my-2 mr-10 text-white uppercase text-2xl font-extrabold pt-40'>
+                <Link to="/">
+                    <img src={latinosvip} alt="Logo" className='text-3xl size-60 w-30 ml-2 h-30 rounded-2xl' />
+                </Link>
+            </div>
+            <div>      
+                <h1 className="text-uppercase font-extrabold text-white animate-pulse">
+                    Bienvenidos a LatinosVip
+                </h1>
+            </div>
+            <div className='  my-2 mr-10 text-white uppercase text-2xl font-extrabold pt-40'>
                 <h1 className="ml-20  hover:text-yellow-200 shadow-xl  ">
                     <FontAwesomeIcon icon={faInstagram} /> LatinosVip1
                 </h1>
             </div>
+        </header>
+        <main className="mx-auto  max-w-6xl py-10 bg-black shadow">
+                    <Outlet/>
+        </main>
+    </>
+  )
+}
+
             {/* <nav className='flex flex-wrap justify-end sm:justify-start  mr-2 ml-2 my-2 text-white uppercase text-2xl  shadow-xl'>
                 <Link to="/inicio" className='mx-2  hover:text-yellow-200'>Barberos</Link>
 
@@ -36,10 +50,3 @@ export default function Layout() {
 
             //     <Link to="/contacto" className='mx-2  hover:text-yellow-200'>Contacto</Link> */}
             {/* // </nav> */}
-        </header>
-        <main className="mx-auto  max-w-6xl py-10 bg-black shadow">
-                    <Outlet/>
-        </main>
-    </>
-  )
-}
